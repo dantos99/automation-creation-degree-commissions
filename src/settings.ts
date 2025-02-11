febdc7306590824de35f9c2407008b6bc404a47e
dantos99 <danytoschi99@gmail.com>
2025-02-11 12:06:14 +0100

    delete directory

--- src/settings.ts
+++
@@ -1,8 +0,0 @@
-class settings {
-
-    //Mostra la pagina di impostaizioni dell'utente
-    static showSettingFile() {
-        let html = HtmlService.createHtmlOutputFromFile('html/setting.html').setWidth(900).setHeight(500).setSandboxMode(HtmlService.SandboxMode.IFRAME);
-        SpreadsheetApp.getUi().showModalDialog(html, " ");
-    }
-}
